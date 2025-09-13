d5d3c6f9c8fc1b5488fac5e69d24ea8693988928
Denis Shiryaev <d.shiryaev111@yandex.ru>
2025-09-13 19:15:41 +0300

    add assets

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+     static var ypGreen: UIColor { UIColor(named: "Green") ?? UIColor.green }
+     static var ypRed: UIColor { UIColor(named: "Red") ?? UIColor.red }
+     static var ypBlack: UIColor { UIColor(named: "Black") ?? UIColor.black }
+     static var ypBackground: UIColor { UIColor(named: "Background") ?? UIColor.darkGray }
+     static var ypGray: UIColor { UIColor(named: "Gray") ?? UIColor.gray }
+     static var ypWhite: UIColor { UIColor(named: "White") ?? UIColor.white }
+}
